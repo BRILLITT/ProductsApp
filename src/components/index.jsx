293ae0a2dd382f0a000fea0e0c423c1cf54bc6d9
BRILLITT <98791144+BRILLITT@users.noreply.github.com>
2022-08-12 04:18:29 -0500

    Delete src directory

--- src/components/index.jsx
+++
@@ -1,2 +0,0 @@
-export {default as NavBar} from './NavBar'
-export { default as LoadingScreen} from './LoadingScreen'
--- src/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Home } from '/Home';
-export { default as Login } from '/Login';
-export { default as Purchases } from '/Purchases';
-export { default as ProductDetail } from '/ProductDetail';
