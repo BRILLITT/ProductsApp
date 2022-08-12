9e1bb74d08893d210ef094000dc473165f636817
BRILLITT <98791144+BRILLITT@users.noreply.github.com>
2022-08-12 04:14:07 -0500

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-
-import { HashRouter, Routes,Route } from "react-router-dom";
-import { Home, Login, ProductsDetail, Purchases} from './pages'
-import { NavBar, LoadingScreen } from "./components";
-import { useSelector } from 'react-redux';
-import { Container} from 'react-bootstrap'
-import './App.css'
-
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import {Home, Purchases, Login, ProductsDetail} from './pages'
+import "./App.css";
 
 function App(){
 
-    const isLoading = useSelector(state => state.isLoading)
-
 return (
-    <HashRouter>
-      <NavBar/>
-      {isLoading && <LoadingScreen/>}
-      <Container>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="product/:id" element={<ProductsDetail />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/purchases" element={<Purchases />} />
-      </Routes>
-      </Container>
-    </HashRouter>
-    )
-}
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="products/:id" element={<ProductsDetail/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/purchases" element={<Purchases/>}/>
+            </Routes>
+        </HashRouter>
+    );
+};
+
 
  export default App;
--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -4,7 +4,6 @@ const Purchases = () => {
     return (
         <div>
             <h1>Purchases</h1>
-            
         </div>
     );
 };
